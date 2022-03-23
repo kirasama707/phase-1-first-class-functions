ac2381505997f68df18b70ad47e04d89fe68a1f7
christian <vagetto707@aol.com>
2022-03-22 19:56:50 -0700

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(paraFunction) {
+    paraFunction()
+}
+function returnsANamedFunction() {
+    return function test() {
+
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        
+    }
+}
